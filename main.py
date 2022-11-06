3a888b99b5a332415479dac8a8c5e3947159625b
RuDimka <94859049+RuDimka@users.noreply.github.com>
2022-11-06 17:48:39 +0300

    #

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # name = input('Enter your name: ')
 # print('Hello', name)
 
-print(1, 2, 3, sep=":")
-print(3, 2, 1, sep="x", end='\n')
-print(2, 5, 6)
+# print(1, 2, 3, sep="ggh", end=':')
+# print(6, 6, 7, sep="kkk", end='\n') #убирает перенос строк, работает без --end--
+# print(9, 8, 7, sep="y")
+
+
